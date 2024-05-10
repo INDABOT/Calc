a4d6d1d090714c20b66313d618c569ab29daa87c
INDABOT <okanuchinonso1.com>
2024-05-10 09:26:37 +0000

    first commit?

---
+++ script.js
@@ -0,0 +1,23 @@
+let displayed = document.getElementById("display");
+
+function display(num){
+    if (displayed.value === "0" && num !== ".") {
+        displayed.value = num;
+    } else {
+        displayed.value += num;
+    }
+}
+
+function Calculate() {
+    try {
+        displayed.value = eval(displayed.value);
+    } catch (err) {
+        alert("Invalid Expression");
+    }
+}
+//incase I decide to add thr clear button
+function clearDisplay() {
+    displayed.value = "0";
+}
+
+
